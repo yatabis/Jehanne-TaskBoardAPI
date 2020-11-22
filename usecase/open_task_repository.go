1c1e703edb292fb7ed3b241aece6d78cf865229f
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-11-22 15:48:37 +0900

    add: OpenTaskRepository とモックの追加

---
+++ usecase/open_task_repository.go
@@ -0,0 +1,9 @@
+//go:generate mockgen -source=open_task_repository.go -destination ../mock_usecase/mock_open_task_repository.go
+
+package usecase
+
+import "github.com/yatabis/Jehanne/TaskBoard/domain"
+
+type OpenTaskRepository interface {
+	FindAll() ([]*domain.OpenTask, error)
+}
